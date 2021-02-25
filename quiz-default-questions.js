7da036db5e194983327954a8c49f326340ec43ee
Srushtika <n.srushtika@gmail.com>
2021-02-25 13:52:27 +0000

    Questions updated

--- quiz-default-questions.js
+++ quiz-default-questions.js
@@ -1,74 +1,34 @@
 module.exports = [
   {
     questionNumber: 1,
-    question:
-      'By default, most web browsers use which colour to denote a hyperlink?',
-    choices: ['Blue', 'Green', 'Maroon', 'Red'],
+    question: 'Which of these is a programming language?',
+    choices: ['Itch', 'Scratch', 'Bite', 'Gnaw'],
     showImg: true,
-    correct: 0,
+    correct: 1,
     pic:
-      'http://www.martinimanna.com/wp-content/uploads/2016/09/New-Mobile-Hyperlink-Allows-Apps-To-Interact-Like-Webpages.jpg'
+      'https://user-images.githubusercontent.com/5900152/109162582-486bd100-7770-11eb-96c9-2e3c41b055f6.jpg'
   },
   {
     questionNumber: 2,
-    question: 'Which of the following shooter series is published by EA?',
-    choices: ['Call Of Duty', 'Fortnite', 'Halo', 'Battlefield'],
+    question: 'Node.js is written in',
+    choices: ['JavaScript', 'C++', 'C', 'All of the above'],
     showImg: true,
     correct: 3,
-    pic: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1238840/header.jpg'
+    pic:
+      'https://user-images.githubusercontent.com/5900152/109162576-46097700-7770-11eb-925b-e1fba754b0e4.jpg'
   },
   {
     questionNumber: 3,
-    question: 'Which of these is largest in memory terms',
-    choices: ['Megabyte', 'Gigabyte', 'Terabyte', 'Kilobyte'],
-    showImg: true,
-    correct: 2,
-    pic: 'https://i.ytimg.com/vi/-aVhta7db2o/maxresdefault.jpg'
-  },
-  {
-    questionNumber: 4,
-    question:
-      'What is a malicious program that is disguised as a legitimate software?',
-    choices: ['Nemean Lion', "Thor's Hammer", 'Trojan Horse', 'Golden Fleece'],
+    question: 'What does NPM stand for?',
+    choices: [
+      'New Package Manager',
+      'New Project Manager',
+      'Node Package Manager',
+      'Node Project Manager'
+    ],
     showImg: true,
     correct: 2,
     pic:
-      'https://www.hs-academypages.com/hubfs/lp/academy/trojan.png?t=1533931401700'
-  },
-
-  {
-    questionNumber: 5,
-    question: '"E" in Amazon\'s EC2 stands for?',
-    choices: ['Eclipse', 'Extra', 'Elastic', 'Economical'],
-    showImg: true,
-    correct: 2,
-    pic:
-      'https://image.slidesharecdn.com/introductiontoamazonec2-150417052810-conversion-gate01/95/introduction-to-amazon-ec2-2-638.jpg?cb=1429250596'
-  },
-  {
-    questionNumber: 6,
-    question: 'What was the code name of Windows 95?',
-    choices: ['Cairo', 'Washington', 'Whistler', 'Chicago'],
-    showImg: true,
-    correct: 3,
-    pic: 'http://chicago.win95.googlepages.com/chic58s-1.gif'
-  },
-  {
-    questionNumber: 7,
-    question: 'Which of these holds true for websockets',
-    choices: ['Bi-directional', 'Duplex', 'Persistent', 'All of the these'],
-    showImg: true,
-    correct: 3,
-    pic:
-      'https://assets.ably.io/assets/diagrams/realtime-intro-7dfe7b01a3c0e0e5a11b7580e8f026e9d88a681fad691ce9f46ba9f4be3bdaee.png'
-  },
-  {
-    questionNumber: 8,
-    question: "What is another term for 'bespoke' software?",
-    choices: ['Custom', 'Free', 'Expensive', 'Open Source'],
-    showImg: true,
-    correct: 0,
-    pic:
-      'https://www.hexacta.com/wp-content/uploads/2016/09/custom-vs-packaged_productivity2.jpg'
+      'https://user-images.githubusercontent.com/5900152/109162581-486bd100-7770-11eb-8941-1e2a8f5dc828.jpeg'
   }
 ];
