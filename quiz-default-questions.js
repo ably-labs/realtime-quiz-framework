6bb846a156965f37204c0b402616ac3ddce171e1
Srushtika <n.srushtika@gmail.com>
2021-03-03 10:07:23 +0000

    New default questions

--- quiz-default-questions.js
+++ quiz-default-questions.js
@@ -1,34 +1,42 @@
 module.exports = [
   {
     questionNumber: 1,
-    question: 'Which of these is a programming language?',
-    choices: ['Itch', 'Scratch', 'Bite', 'Gnaw'],
+    question: 'How fast is the blink of an eye?',
+    choices: ['2 seconds', '1 second', '0.5 seconds', '0.2 seconds'],
     showImg: true,
-    correct: 1,
-    pic:
-      'https://user-images.githubusercontent.com/5900152/109162582-486bd100-7770-11eb-96c9-2e3c41b055f6.jpg'
+    correct: 3,
+    pic: 'https://media.giphy.com/media/2AX09OcYVEsUM/giphy.gif'
   },
   {
     questionNumber: 2,
-    question: 'Node.js is written in',
-    choices: ['JavaScript', 'C++', 'C', 'All of the above'],
+    question:
+      "How fast is Ably? What is Ably's average roundtrip latency globally?",
+    choices: ['1 second', '3 seconds', '0.065 seconds', '0.001 seconds'],
     showImg: true,
-    correct: 3,
-    pic:
-      'https://user-images.githubusercontent.com/5900152/109162576-46097700-7770-11eb-925b-e1fba754b0e4.jpg'
+    correct: 2,
+    pic: 'https://media.giphy.com/media/7lz6nPd56aHh6/giphy.gif'
   },
   {
     questionNumber: 3,
-    question: 'What does NPM stand for?',
+    question:
+      'How many children moved to remote interactive digitial learning in 2020?',
+    choices: ['1.21 billion', '568 million', '3.46 billion', '1.57 billion'],
+    showImg: true,
+    correct: 3,
+    pic: 'https://media.giphy.com/media/4GZyVJkdSHTImp74RP/giphy.gif'
+  },
+  {
+    questionNumber: 4,
+    question:
+      'What does Ably not yet power for children learning via online EdTech?',
     choices: [
-      'New Package Manager',
-      'New Project Manager',
-      'Node Package Manager',
-      'Node Project Manager'
+      'Online proctoring',
+      'Collaborative whiteboards',
+      'Telepathy with their friends',
+      'Chat'
     ],
     showImg: true,
     correct: 2,
-    pic:
-      'https://user-images.githubusercontent.com/5900152/109162581-486bd100-7770-11eb-8941-1e2a8f5dc828.jpeg'
+    pic: 'https://media.giphy.com/media/q6gzszRDonw3u/giphy.gif'
   }
 ];
